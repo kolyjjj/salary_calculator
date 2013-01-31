e265453efa91833767398abd1f64d21c7f48e33c
kolyjjj <me.koly.li@gmail.com>
2013-01-31 12:59:59 +0800

    [Koly] Refactor

--- src/koly/li/Kengdie.java
+++ src/koly/li/Kengdie.java
@@ -3,26 +3,31 @@ package koly.li;
 public class Kengdie {
 
     private final SalaryCalculator calculator;
+    private double salary;
 
     public Kengdie(SalaryCalculator calculator) {
         this.calculator = calculator;
     }
 
     public String calculate(int days) {
-        double salary = calculator.acquireSalary(days, 200.);
-        double finalSalary = salary - calculator.acquirePersonalTax(salary, 0.1, 3500.);
-        double finalBonus = calculateFinalBonus(calculator.acquireSalary(days, 200.));
+        salary = calculator.acquireSalary(days, 200.);
+        double finalSalary = calculateFinalSalary();
+        double finalBonus = calculateFinalBonus();
 
         return  "salary:" + finalSalary + "\n" +
                 "bonus:" + finalBonus + "\n" +
                 "income:" + calculateIncome(finalSalary, finalBonus) + "\n";
     }
 
+    private double calculateFinalSalary() {
+        return salary - calculator.acquirePersonalTax(salary, 0.1, 3500.);
+    }
+
     private double calculateIncome(double salary, double bonus) {
         return salary + bonus;
     }
 
-    private double calculateFinalBonus(double salary){
+    private double calculateFinalBonus(){
         double bonus = calculator.acquireBonus(salary, 0.1);
         return bonus - calculator.acquireBonusTax(bonus, 0.05);
     }
--- test/koly/li/kengdieTest.java
+++ test/koly/li/kengdieTest.java
@@ -1,20 +1,27 @@
 package koly.li;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class KengdieTest {
+
+    private Kengdie kengdie;
+
+    @Before
+    public void setUp() throws Exception {
+        kengdie = new Kengdie(new SalaryCalculator());
+    }
+
     @Test
     public void should_return_salary_bonus_and_total_income() throws Exception {
-        Kengdie kengdie = new Kengdie(new SalaryCalculator());
         assertThat(kengdie.calculate(10), is("salary:2000.0\nbonus:190.0\nincome:2190.0\n"));
     }
 
     @Test
     public void should_return_salary_bonus_and_total_income_when_salary_beyond_limit() throws Exception {
-        Kengdie kengdie = new Kengdie(new SalaryCalculator());
         assertThat(kengdie.calculate(30), is("salary:5750.0\nbonus:570.0\nincome:6320.0\n"));
     }
 }
