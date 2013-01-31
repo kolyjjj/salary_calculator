35e6c45ffab53b86a8084fbd24aefeca32bb1e43
kolyjjj <me.koly.li@gmail.com>
2013-01-31 11:33:35 +0800

    [Koly] Refactored the calcualtion of final bonus

--- src/koly/li/Kengdie.java
+++ src/koly/li/Kengdie.java
@@ -1,22 +1,32 @@
 package koly.li;
 
 public class Kengdie {
+
+    private final SalaryCalculator calculator;
+
+    public Kengdie(SalaryCalculator calculator) {
+        this.calculator = calculator;
+    }
+
     public String calculate(int days) {
         String result = "";
-        SalaryCalculator calculator = new SalaryCalculator();
         //calculateSalary
         double salary = calculator.acquireSalary(days, 200.);
         result += "salary:" + salary + "\n";
 
         //calculateBonus
-        double bonus = calculator.acquireBonus(salary, 0.1);
-        double finalBonus = bonus - calculator.acquireBonusTax(bonus, 0.05);
+        double finalBonus = calculateFinalBonus(salary);
         result += "bonus:" + finalBonus + "\n";
 
         //calculateIncome
-        double income = salary + bonus + calculator.acquirePersonalTax(salary, 0.1, 3500) -
-                calculator.acquireBonusTax(bonus,0.05);
+        double income = salary + finalBonus + calculator.acquirePersonalTax(salary, 0.1, 3500);
         result += "income:" + income + "\n";
         return result;
+    }
+
+    private double calculateFinalBonus(double salary){
+        double bonus = calculator.acquireBonus(salary, 0.1);
+        double finalBonus = bonus - calculator.acquireBonusTax(bonus, 0.05);
+        return finalBonus;
     }
 }
